f3c83fbb9c8ea1945cf2d7246b4637b8f9f2de85
Adrien Auré <adrien.aure@gmail.com>
2026-02-26 13:20:14 +0100

    Removed sounds

--- script.js
+++
@@ -1,9 +0,0 @@
-const yeah = new Audio('src/sounds/sound_yeah.mp3');
-yeah.volume = 0.1;
-document.querySelectorAll('.tildes, .title').forEach(el => {
-  el.style.cursor = 'pointer';
-  el.addEventListener('click', () => {
-    yeah.currentTime = 0;
-    yeah.play();
-  });
-});
